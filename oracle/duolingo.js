16dbc901835b5c219c0171b8a6480be27c50b6c7
mm-near <michalski@near.org>
2022-12-31 15:50:33 +0100

    small fixes to duolingo

--- oracle/duolingo.js
+++ oracle/duolingo.js
@@ -16,7 +16,7 @@ const myKeyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
 
 
 
-const connectionConfig = {
+const testnetConnectionConfig = {
     networkId: "testnet",
     keyStore: myKeyStore, // first create a key store 
     nodeUrl: "https://rpc.testnet.near.org",
@@ -24,13 +24,34 @@ const connectionConfig = {
     helperUrl: "https://helper.testnet.near.org",
     explorerUrl: "https://explorer.testnet.near.org",
 };
+
+const testnetContractAccount = "dev-1671218049971-36407954871979"
+
+
+const mainnetConnectionConfig = {
+    networkId: "mainnet",
+    keyStore: myKeyStore, // first create a key store 
+    nodeUrl: "https://rpc.near.org",
+    walletUrl: "https://wallet.near.org",
+    helperUrl: "https://helper.near.org",
+    explorerUrl: "https://explorer.near.org",
+};
+
+const mainnetContractAccount = "language.betteryou.near"
+
+
+
+const connectionConfig = testnetConnectionConfig;
+const contractAccount = testnetContractAccount;
+
+
 const nearConnection = await connect(connectionConfig);
-const account = await nearConnection.account("language.betteryou.testnet");
+const account = await nearConnection.account(contractAccount);
 
 
 const contract = new Contract(
     account, // the account object that is connecting
-    "language.betteryou.testnet",
+    contractAccount,
     {
         // name of contract you're connecting to
         viewMethods: ["get_all_state"], // view methods do not change state but usually return a value
@@ -43,17 +64,35 @@ async function run() {
     let updateArray = [];
     let state = await contract.get_all_state();
     for (const challenge of state) {
-        const duo = new Duo(challenge[1].duolingo_username.toString());
-        const courses = await duo.getCourses();
-        const result = courses.find(({ title }) => title === challenge[1].language);
-        if (result) {
-            updateArray.push([challenge[0], result.xp]);
+        try {
+            //const duo = new Duo(challenge[1].duolingo_username.toString());
+            const duo = new Duo("fsdkfjsdlfkj");
+
+            const courses = await duo.getCourses();
+            const result = courses.find(({ title }) => title === challenge[1].language);
+            if (result) {
+                if (result.xp == challenge[1].total_xp) {
+                    console.log("No update for " + challenge[0] + " still at " + result.xp);
+                } else {
+                    updateArray.push([challenge[0], result.xp]);
+                }
+            } else {
+                console.error(`Cannot find language ${challenge[1].language} for user ${challenge[1].duolingo_username} (${challenge[0]})`)
+            }
+        } catch (error) {
+            console.error(`Failed to fetch Duolingo for ${challenge[1].language} for user ${challenge[1].duolingo_username} (${challenge[0]})`)
         }
+
     }
     console.log(updateArray);
-    try { await contract.admin_update_challenge({ update: updateArray }) } catch (e) {
-        console.log(e);
-    };
+    // Call the contract only if there are some updates.
+    if (updateArray.length > 0) {
+        try { await contract.admin_update_challenge({ update: updateArray }) } catch (e) {
+            console.log(e);
+        };
+    } else {
+        console.log("Skipping");
+    }
 }
 
 
@@ -93,7 +132,7 @@ function request(self, url) {
  * @returns {Promise}
 */
 function getData(self) {
-    var url = base + encodeURIComponent(`https://www.duolingo.com/2017-06-30/users?username=${self.username}`);
+    var url = base + encodeURIComponent(`https://www.duolingo.com/2017-06-30/users?username=${self.username}&ts=${new Date().getTime()}`);
     return request(self, url).then(res => {
         var data = JSON.parse(res.contents).users[0];
         if (!data) throw new Error("Invalid username");
@@ -111,8 +150,18 @@ Duo.prototype.getCourses = function () {
     });
 }
 
+
+
+console.log("starting first run.");
+run();
+
+let date_ob = new Date();
+console.log(`Setting up cron ${date_ob.toUTCString()}`);
+
 //cron job that runs the update on the contract every 10 minutes
-cron.schedule('10 * * * *', () => {
-    console.log("Running the cron job")
+cron.schedule('*/10 * * * *', () => {
+    let date_ob = new Date();
+
+    console.log(`Running the cron job ${date_ob.toUTCString()}`)
     run();
 });
